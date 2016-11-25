f4b0d172afe163752876ead41e993785e4bb05e7
JINWOO1225 <jinwoo17962@gmail.com>
2016-11-25 20:13:50 +0900

    Update_main.c

--- C_tetris/main.c
+++ C_tetris/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void)
 {
+	int row,col;
+	
 	bool J_shape[3][3] = {{0,0,1},  //001
 			     {0,0,1},  //001
 			     {0,1,1}}; //011
@@ -32,7 +33,14 @@ int main(void)
 	bool Z_shape[3][3] = {{1,1,0},    //110
 			      {0,1,1},    //011
 			      {0,0,0}};   //000
-						  
-	
+	for(row = 0; row < 3; row++)
+	{
+		for(col = 0; col < 3; col++)
+		{
+			printf("%2d",J_shape[row][col]);
+		}
+		printf("\n");
+	}
 	return 0;
 }
+

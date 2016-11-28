aa545a40121f09fd59f42a3fcc751b9279e95955
JINWOO1225 <jinwoo17962@gmail.com>
2016-11-29 07:28:28 +0900

    Update main.c

--- C_tetris/main.c
+++ C_tetris/main.c
@@ -8,34 +8,54 @@
 #include <stdlib.h>
 #include <stdbool.h>  //bool data type 
 #include <conio.h>    //input without buffer 
+#include <time.h>     
 
 extern bool shape[7][4][4][4];
 //4 Dimension array from shapes.c
 int main(void)
 {
 	int style = 0,dir = 0,row,col;
+	char ch;
+	srand(time(NULL));
 	
-	for(style = 0; style < 7; style++)
-	{
-		for(dir = 0; dir < 4; dir++)
-		{
-			printf("STYLE : %d\tDIRECTION : %d\n",style,dir);
-			for(row = 0; row < 4; row++)
-			{
-				for(col = 0; col < 4; col++)
-				{
-					if(shape[style][dir][row][col])
-					{
-						printf("= ");
-					}
-					else
-					{
-						printf("  ");
-					}
-				}
-				printf("\n");
-			}
+	while(ch != '#')
+	{   //to stop press '#'
+		if(ch == 'n')
+		{   //for new style press 'n'
+			style = rand() % 7;
+			printf("New Style! : %d\n",style);
 		}
+		for(row = 0; row < 4; row++)
+		{
+			for(col = 0; col < 4; col++)
+			{
+				if(shape[style][dir][row][col])
+					printf("= ");
+				else
+					printf("  ");
+			}
+			printf("\n");
+		}
+		ch = getch();
+		
+		switch(ch)
+		{
+			case 'q':
+				//pressing 'q' to
+				printf("going right");
+				dir++;
+				break;
+			case 'e':
+				//pressing 'e' to
+				printf("going left");
+				dir--;
+				break;
+			default:
+				break;
+		}
+		if(dir == -1)
+			dir = 3;  //dir must go -> 0 1 2 3 0 1 2 3 0 1 2 3 
+		dir = dir % 4;
 	}
 	return 0;
 }

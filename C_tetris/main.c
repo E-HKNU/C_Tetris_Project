4a261c74ea4db3b2a642c062485741a6a40312ee
JINWOO1225 <jinwoo17962@gmail.com>
2016-11-29 13:12:15 +0900

    main

--- C_tetris/main.c
+++ C_tetris/main.c
@@ -1,6 +1,7 @@
 //filename : main.c
 // 2016/11/28
-// Displaying every shape of TETRIS
+// Displaying, rotating every shape of TETRIS
+//
 // 2016250055 Jinwoo  Hong
 // 2016250033 Wooseob Byeon
 
@@ -17,21 +18,23 @@ extern bool shape[7][4][4][4];
 //4 Dimension array from shapes.c
 int main(void)
 {
-	int style = 0,dir = 0,row,col;
+	int style = 0,dir = 0;
 	char ch;
+	
 	srand(time(NULL));
 	
 	while(ch != '#')
 	{   //to stop press '#'
 		if(ch == 'n')
-		{   //for new style press 'n'
 			style = rand() % 7;
-		}
+		    //press 'n' for new style
 		display(style,dir);
-		
+		//display current shape
 		ch = getch();
-		
+		//bufferless input
 		rotate(ch, &dir);
+		//when getch() gets 'q','e'
+		//rotate current shape to clock, anticlock wise 
 	}
 	return 0;
 }
@@ -58,17 +61,18 @@ void rotate(char ch, int *dir)
 {
 	switch(ch)
 	{
-		case'q':
-			(*dir)--;
-			break;
 		case'e':
 			(*dir)++;
-			break;
+			break; //clockwise
+		case'q':
+			(*dir)--;
+			break; //anticlockwise
 	}
 	if(*dir == -1)
 		*dir = 3;
 	*dir = *dir % 4;
-	//dir must go 0 1 2 3 0 1 2 3
+	//dir must go 3 0 1 2 3 0
+	//           -1 0 1 2 3 4
 	return;
 }
 
